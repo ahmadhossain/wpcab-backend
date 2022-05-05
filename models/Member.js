bfd7c80b18283ec419bec635d51dee492b962e56
Ahmad Hossain <ahmadhossain.bu@gmail.com>
2022-05-05 11:20:51 +0600

    prev code add

--- models/Member.js
+++ models/Member.js
@@ -1,17 +1,6 @@
+const mongoose = require("mongoose");
 
-const express = require("express");
-const mongoose = require('mongoose');
-const app = express();
-const dotenv = require("dotenv");
-  
-app.listen(process.env.PORT || 2000, function() {
-  console.log("Server started on port 2000");
-});
-// mongodb://localhost:27017/wpcab_DB
-dotenv.config();
-mongoose.connect(process.env.MONGO_URL);
-
-const UserSchema = new mongoose.Schema({
+const MemberSchema = new mongoose.Schema({
     _id : String,
     FullName : String,
     Gender : String,
@@ -61,37 +50,4 @@ const UserSchema = new mongoose.Schema({
     Role : String
 });
 
-const User = new mongoose.model("Member", UserSchema);
-
-app.get("/", function(req,res){
-    res.send("<h1>Welcome to WPCAB server!</h1>");
-})
-
-app.get("/users", function(req,res){
-    User.find({ PermanentAddressDristrict: "ঢাকা" },function(err,found){
-      res.send(found);
-    })
-});
-
-
-app.post("/users", function(req,res){
-    var district = req.body.district;
-
-    User.find({PermanentAddressDristrict: district},function(err,found){
-          res.send(found);
-    })
-});
-
-app.post("/login", function(req,res){
-  var username = req.body.field;
-  var password = req.body.value;
-  
-  if(username==="wpcab.admin" && password==="admin@wpcab"){
-    res.send(true);
-  }
-  else res.send(false);
-
-});
-
-
-
+module.exports = mongoose.model("Member", MemberSchema);
